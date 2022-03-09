54a907983b57db6818a59c2d0e780ee850c06fc9
aanmeba <aanmeba@gmail.com>
2022-03-09 18:18:31 +1100

    add input validation

--- app/models/option.rb
+++ app/models/option.rb
@@ -2,4 +2,7 @@ class Option < ApplicationRecord
   belongs_to :project
 
   validates_format_of :title, without: /\W/, allow_blank: true
+  validates :title, :description, :amount, presence: true
+  validates :amount, numericality: { only_integer: true }
+
 end
--- app/models/project.rb
+++ app/models/project.rb
@@ -5,13 +5,19 @@ class Project < ApplicationRecord
 
   has_one_attached :picture
 
-  validate :due_date_should_be_after_start_date  
   validates :title, :description, :start_date, :due_date, :goal_amount, :category_id, presence: true
+  validate :due_date_should_be_after_start_date  
   validates :goal_amount, numericality: { only_integer: true }
 
   def due_date_should_be_after_start_date
-    if due_date > start_date && due_date < Date.today
-      errors.add(:due_date, "can't be before start date and before today")
+    if start_date && due_date
+      if (start_date > due_date) || (due_date < Date.today)
+        errors.add(:due_date, "can't be before start date and before today")
+      elsif start_date == due_date
+        errors.add(:due_date, "should be after start date")
+      # elsif due_date = Date.today
+      #   errors.add(:due_date, "can't be today")
+      end
     end
   end
   
