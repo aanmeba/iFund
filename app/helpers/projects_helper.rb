90d093e371942026076bc6889e14dff3281f0009
aanmeba <aanmeba@gmail.com>
2022-03-11 13:33:39 +1100

    create project helper for gererating image url

--- app/helpers/projects_helper.rb
+++ app/helpers/projects_helper.rb
@@ -3,4 +3,21 @@ module ProjectsHelper
     "$#{(price/100.0)}"
   end
 
+  def image_link(temp)
+    @base_url = "https://ja-ifund-rails-app.s3.ap-southeast-2.amazonaws.com/"
+    @location = "default/default_"
+    @key = @categories.find(temp.category_id).name
+    @extension = ".jpeg"
+    
+    begin
+      if Rails.env.development?
+        @location + @key + @extension
+      elsif Rails.env.staging? || Rails.env.production?
+        @base_url + @location + @key + @extension
+      end  
+    rescue => exception
+      "#"
+    end
+
+  end
 end
