84d6b2ef04cff23e4784de8e32b184e6fb467ba7
aanmeba <aanmeba@gmail.com>
2022-03-11 17:42:47 +1100

    remove staging environment

--- config/application.rb
+++ config/application.rb
@@ -22,8 +22,8 @@ module IFund
     config.autoload_paths += %W(#{Rails.root}/app/models/users)
 
     # add support for custom environments in heroku
-    if ENV["PIPE_ENV"].present?
-      Rails.application.config.credentials.content_path = Rails.root.join("config/credentials/#{ENV["PIPE_ENV"]}.yml.enc")
-    end
+    # if ENV["PIPE_ENV"].present?
+    #   Rails.application.config.credentials.content_path = Rails.root.join("config/credentials/#{ENV["PIPE_ENV"]}.yml.enc")
+    # end
   end
 end
