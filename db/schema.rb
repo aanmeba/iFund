d018035d3138d6451a9f489867cdaa50a4e5189b
aanmeba <aanmeba@gmail.com>
2022-03-15 18:05:51 +1100

    add status column in projects table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_09_035141) do
+ActiveRecord::Schema.define(version: 2022_03_15_062436) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -71,6 +71,7 @@ ActiveRecord::Schema.define(version: 2022_03_09_035141) do
     t.datetime "updated_at", precision: 6, null: false
     t.integer "amount", default: 0
     t.integer "total_amount", default: 0
+    t.boolean "status"
     t.index ["category_id"], name: "index_projects_on_category_id"
     t.index ["user_id"], name: "index_projects_on_user_id"
   end
