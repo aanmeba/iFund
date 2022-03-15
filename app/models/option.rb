de367dfafa443799890790c4c901ad066fbf31ae
aanmeba <aanmeba@gmail.com>
2022-03-15 14:53:42 +1100

    add skip and back button in option views

--- app/models/option.rb
+++ app/models/option.rb
@@ -1,7 +1,6 @@
 class Option < ApplicationRecord
   belongs_to :project
 
-  validates_format_of :title, without: /\W/, allow_blank: true
   validates :title, :description, :amount, presence: true
   validates :amount, numericality: { only_integer: true }
 
