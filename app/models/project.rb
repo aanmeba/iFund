dbb02b73a2dbde6b56762be0e284793c569d0bb2
aanmeba <aanmeba@gmail.com>
2022-03-17 18:29:49 +1100

    add before_validation to convert dollars to cents

--- app/models/project.rb
+++ app/models/project.rb
@@ -7,9 +7,17 @@ class Project < ApplicationRecord
   # has_one_attached :picture
   has_one_attached :picture, service: :amazon
 
+  before_validation :dollar_to_cents
+
   validates :title, :description, :start_date, :due_date, :goal_amount, :category_id, presence: true
   validate :due_date_should_be_after_start_date  
   validates :goal_amount, numericality: { only_integer: true }
+
+  def dollar_to_cents
+    if !goal_amount.nil? && goal_amount != 0
+      self.goal_amount = (self.attributes_before_type_cast["goal_amount"].to_f * 100).round
+    end
+  end
 
   def due_date_should_be_after_start_date
     if start_date && due_date
