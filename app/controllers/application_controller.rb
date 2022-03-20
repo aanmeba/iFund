fc8ee33be115a0787db42cc83035dfe8b53b4d5a
aanmeba <aanmeba@gmail.com>
2022-03-20 18:48:53 +1100

    uncomment custom error route and method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   # when clicking back in browser on payment page
   # /payments?id= should be POST method, but sometimes it behaves GET
   # it causes routing error occurs 
-  def route_not_found
-    render file: Rails.public_path.join('404.html'), status: :not_found, layout: true
-  end
+  # def route_not_found
+  #   render file: Rails.public_path.join("404.html"), status: :not_found, layout: true
+  # end
 end
